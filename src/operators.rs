fa87b9655a4a559c3126668bedccb0980015e9e7
Hangjie Mo <mohangjie1995@gmail.com>
2025-01-10 17:05:18 +0800

    supports SwiGLU

--- src/operators.rs
+++ src/operators.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 use crate::tensor::Tensor;
 
 // get (row) vectors from a 2D table given a list of indices
@@ -77,13 +79,15 @@ pub fn rms_norm(y: &mut Tensor<f32>, x: &Tensor<f32>, w: &Tensor<f32>, epsilon: 
 // y = silu(x) * y
 // hint: this is an element-wise operation
 pub fn swiglu(y: &mut Tensor<f32>, x: &Tensor<f32>) {
-    // let len = y.size();
-    // assert!(len == x.size());
+    let len = y.size();
+    assert!(len == x.size());
 
-    // let _y = unsafe { y.data_mut() };
-    // let _x = x.data();
+    let _y = unsafe { y.data_mut() };
+    let _x = x.data();
 
-    todo!("实现 silu，这里给了一些前期准备工作的提示，你可以参考")
+    for i in 0..len {
+        _y[i] = _x[i] / (1.0 + (-_x[i]).exp()) * _y[i];
+    }
 }
 
 // C = beta * C + alpha * A @ B^T
